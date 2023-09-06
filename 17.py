2dc31331e33a682a1fe3bb0eca6506b1323d7b0a
Ravi Kiran Kunsoth <44895351+kravikiraniitb@users.noreply.github.com>
2023-09-06 17:19:09 -0400

    new file:   17.py

---
+++ 17.py
@@ -0,0 +1,9 @@
+def letterCombinations(digits):
+    n2 = ["a","b","c"]
+    n3 = ["d","e","f"]
+    n4 = ["g","h","i"]
+    n5 = ["j","k","l"]
+    n6 = ["m","n","o"]
+    n7 = ["p","q","r", "s"]
+    n8 = ["t","u","v"]
+    n9 = ["w","x","y", "z"]

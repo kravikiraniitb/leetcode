3438f6a185df653215ffc50b6d5553acb7396554
Ravi Kiran Kunsoth <44895351+kravikiraniitb@users.noreply.github.com>
2023-09-13 16:03:24 -0400

    working but still with bugs -	modified:   19.py

--- 19.py
+++ 19.py
@@ -3,22 +3,23 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-
-def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+class Solution:
+    
+    
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         arr = []
-        #f = ListNode()
+        f = ListNode()
 
         count = 0
         #current = get_first_element(head)  # Start at the head node
         if head.next is not None:
-            current = head.val
+            current = head
         else:
             current = None  # The list is empty
 
 
         while current is not None:
-            arr.append(current)
+            arr.append(current.val)
             count += 1
             current = current.next  # Move to the next node
 

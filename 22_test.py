fd72ebabe1ff307960715523b63cdeb425e0a2db
Ravi Kiran Kunsoth <44895351+kravikiraniitb@users.noreply.github.com>
2023-09-18 22:10:45 -0400

    modified:   22_test.py

--- 22_test.py
+++ 22_test.py
@@ -1,9 +1,15 @@
-def one_l():
-    ar2_c = ar2.copy()
-    ar2.append("(")
-    ar3.ar2
-    ar2_c.append(")")
-    ar3.ar2
+ar2=[]
+ar3=[]
+def generateParenthesis(n):
+    def one_l():
+        ar2_c = ar2.copy()
+        ar2.append("(")
+        ar3.append(ar2)
+        ar2_c.append(")")
+        ar3.append(ar2_c)
 
     while len(ar3) <= 2**n:
         one_l()
+    return ar3
+
+print(generateParenthesis(2))

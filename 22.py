aad1b7a65b611cfb8c05660d48e4f0b02766c3b2
Ravi Kiran Kunsoth <44895351+kravikiraniitb@users.noreply.github.com>
2023-09-18 21:22:50 -0400

    modified:   22.py

--- 22.py
+++ 22.py
@@ -7,3 +7,19 @@ class Solution:
 
         for x in range(n):
             b_ar.append(")")
+
+        #t = 2**(2*n)
+        arr = []
+        #()()()
+    def factorial(f):
+        if (f == 0 or f ==1):
+            return 1
+
+        return f*factorial(f-1)
+
+        c= int(factorial(2*n)/(factorial(n)*factorial(n)))
+
+        for y in range(c):
+            n=0
+            m=0
+
